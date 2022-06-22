2cad77cd9dd34cdc8ccbb471310d5b6078f63549
Akram <akramshaik1206@gmail.com>
2022-06-22 23:06:49 +0530

    commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants={
+    openWeatherMap: {
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"c6b24de097e818a1939bd07feead76cd"
+    }
+}
+
+module.exports=constants;
